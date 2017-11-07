8350b9c29c0d22f8649d92a819636badc125b885
oliveralonzo <oliveralonzof@gmail.com>
2017-11-07 16:26:21 -0600

    Bundle scheduling code

--- index.php
+++ index.php
@@ -1,60 +1,5 @@
 <?php
-include 'course.php';
-include 'schedule.php';
-
-class Schedules {
-	private $schedules;
-	private $coursesByTitle;
-	private $courseTitles;
-	private $amount;
-	private $maxCredits;
-
-	function __construct($coursesByTitle, $maxCredits) {
-		$this->amount = $amount;
-		$this->coursesByTitle = $coursesByTitle;
-		$this->courseTitles = array_keys($coursesByTitle);
-		$this->maxCredits = $maxCredits;
-		$this->schedules = [];
-		$this->generateSchedules();
-	}
-
-	private function generateSchedules() {
-		$numTitles = count($this->courseTitles);
-		for ($i = 0; $i<$numTitles; $i++) {
-			//echo "restart: " . $i ."<br><br><br><br>";
-			$this->generateSchedulesHelper($i, new Schedule($this->maxCredits));
-		}
-	}
-
-	private function generateSchedulesHelper($currentIndex, $schedule) {
-		//echo "count: ". count($schedule) . "<br>";
-		$numTitles = count($this->courseTitles);
-		if ($currentIndex <= $numTitles) {
-			for ($i = $currentIndex; $i<$numTitles; $i++) {
-				$currentTitle = $this->courseTitles[$i];
-				foreach($this->coursesByTitle[$currentTitle] as $course) {
-					$schedule->addCourse($course);
-					//echo nl2br($schedule). "<br><br>";
-					if ($schedule->full()) {
-						//echo "yes" ."<br><br><br><br>";
-						array_push($this->schedules, clone $schedule);
-						$schedule->removeCourse($course);
-					}
-					$this->generateSchedulesHelper(++$currentIndex, $schedule);
-					$schedule->removeCourse($course);
-				}
-			}
-		}
-	}
-
-	public function getSchedules() {
-		return array_unique($this->schedules);
-	}
-
-	public function __toString() {
-		return implode("\n\n", array_values($this->getSchedules()));
-	}
-}
+include 'schedule-tool.php';
 
 function formatTime($time) {
 	if (strlen($time) == 5) {
@@ -73,15 +18,6 @@ foreach ($file as $line) {
 	array_push($coursesByTitle[$title], new Course($crn, $course, $instructor, $title, $days, $time, $credits, $category));
 }
 
-// foreach($coursesByTitle as $title => $courses) {
-//   echo $title . "<br>";
-//   foreach($courses as $course) {
-// 	  echo $course . "<br>";
-//   }
-//   echo "<br>";
-// }
-
-// echo join("<br>", $courses);
 $schedules = new Schedules($coursesByTitle, 18);
 echo nl2br($schedules);
 echo "<br>".count($schedules->getSchedules());
