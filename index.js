b24d42866e1af3167bc4943a8442b2fb79192490
oliveralonzo <oliveralonzof@gmail.com>
2017-11-26 16:50:58 -0600

    Avoid adding duplicates to cart with Semantic UI Dropdown

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ $(document).ready(function(){
         });
 
         $("#submitByTitle").click(function(){
-            addClassToCart($("#titles"), $("#courseNumbers"));
+            addClassToCart($("#titles").find("option:selected"));
         });
 
         $("#submitByCode").click(function(){
-            addClassToCart($("#courseNumbers"), $("#titles"));
+            addClassToCart($("#courseNumbers").find("option:selected"));
         });
 
         getNumbers($("#codes option:selected").text());
@@ -34,9 +34,10 @@ $(document).ready(function(){
         });
 
         $(".classes-added-list").on("click", ".removeCourse", function() {
-            var courseTitle = $(this).parent().find(".courseTitle").attr("value");
             $(this).parent().remove();
-            enableDropDowns(courseTitle);
+            if (!$(".title").length) {
+              $(".classes-added h1").remove();
+            }
         });
 
         $(".activeRestrictions").on("click", ".removeRestriction", function() {
@@ -67,12 +68,6 @@ function getNumbers(subject_code) {
   getNums.done(function(data){
     var $courseNumbers = $("#courseNumbers");
     $courseNumbers.append(data);
-    $courseNumbers.find("option").each(function() {
-        if ($(".classes-added-list").text().indexOf($(this).prop("title"))>-1) {
-            $(this).attr('disabled','disabled');1
-        };
-    });
-    $courseNumbers.children('option:enabled').eq(0).prop('selected',true);
   })
 
 }
@@ -82,53 +77,35 @@ function toggle($toShow, $toHide) {
     $toHide.hide();
 }
 
-function addClassToCart($main, $other) {
+function addClassToCart($course) {
     if (!$(".title").length) {
         $(".classes-added").prepend("<h1> Classes Selected </h1>");
     }
 
-    var $course = $main.find("option:selected");
     var title = $course.attr("title");
-    var course = $course.attr("course");
-    var credits = $course.attr("credits");
-    var variCredits = '';
+    if ($(".classes-added-list").text().indexOf(title)<0) {
+      var course = $course.attr("course");
+      var credits = $course.attr("credits");
+      var variCredits = '';
 
-    if (credits == "") {
-      variCredits = '<input type="text" maxlength="1" size="2" value="" name="credits">';
+      if (credits == "") {
+        variCredits = '<input type="text" maxlength="1" size="2" value="" name="credits">';
+      }
+
+      $(".classes-added-list").append(
+          '<li class="title"> <span value="'+title+'" credits="'+credits+'"class="courseTitle">'+
+          title+" - "+course+" - "+credits+variCredits+
+          '</span> <span class="removeCourse remove">Remove</span> </li>');
+
+      $('input[name="credits"]').change(function () {
+        var credits = $(this).val();
+        $(this).parent().attr("credits", credits);
+      });
+    } else {
+      // Display message for not being able to add
     }
 
-    $(".classes-added-list").append(
-        '<li class="title"> <span value="'+title+'" credits="'+credits+'"class="courseTitle">'+
-        title+" - "+course+" - "+credits+variCredits+
-        '</span> <span class="removeCourse remove">Remove</span> </li>');
-
-    $('input[name="credits"]').change(function () {
-      var credits = $(this).val();
-      $(this).parent().attr("credits", credits);
-    });
-
-    $course.attr('disabled','disabled');
-    $main.children('option:enabled').eq(0).prop('selected',true);
-
-    $courseInOther = $other.find("option[value='"+$course.val()+"']");
-    if ($courseInOther.length) {
-        $courseInOther.attr('disabled','disabled');
-        $other.children('option:enabled').eq(0).prop('selected',true);
-    }
     initSemantic();
-}
-
-function enableDropDowns(courseTitle) {
-    enableDropDown($("#titles"), courseTitle);
-    enableDropDown($("#courseNumbers"), courseTitle);
-    if (!$(".title").length) {
-      $(".classes-added h1").remove();
-    }
-}
-
-function enableDropDown($dropdown, courseTitle) {
-    $dropdown.find("option[title='"+courseTitle+"']").attr("disabled", false);
-    $dropdown.children('option:enabled').eq(0).prop("selected",true);
 }
 
 function generateSchedules() {
@@ -147,9 +124,11 @@ function generateSchedules() {
     var credits = $("#credits").val();
     var posting = $.post("courseDB.php", { titles: titles.join(" && "), credits: credits, blocks: blocks.join(" && ") });
     posting.done(function(data){
-        //$(".schedules .content").html(data);
-        processSchedules(data);
+        $(".schedules .content").html(data);
+        //processSchedules(data);
+        $(".schedules").modal('refresh');
     });
+    $(".schedules").modal('show');
 }
 
 function addRestriction() {
@@ -218,6 +197,4 @@ function processSchedules(data){
       })
     });
   });
-  $(".schedules").modal('show');
-  $(".schedules").modal('refresh');
 }
